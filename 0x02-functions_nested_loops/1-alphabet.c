ac8e59a9639c53f85277f2e41f157f106ed20b06
Luyanda <luyandamthembu.lm@gmail.com>
2023-06-20 03:29:56 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{       
+    char c;
+
+    for(c = 'a'; c <= 'a'; c++)
+    
+    {
+
+    _putchar(c);
+
+    
+    }
+    
+    _putchar('\n');
+}
